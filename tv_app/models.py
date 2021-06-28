c38fd82e417e9788ada6d8f52149e4989582af62
Cristian Villalobos <cr.villalobos87@gmail.com>
2021-06-28 19:09:10 +0200

    Validated forms

--- tv_app/models.py
+++ tv_app/models.py
@@ -1,10 +1,23 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.db.models.fields import CharField, TextField
+
+class BlogManager(models.Manager):
+    def basic_validator(self, data):
+        errors = {}
+        if len(data['title']) < 2:
+            errors['title'] = 'Show title must be at least 2 characters'
+        if len(data['network']) < 3:
+            errors['network'] = 'Network must be at least 3 characters'
+        if len(data['description']) < 5 and len(data['description']) != 0:
+            errors['description'] = 'Show description must be at least 10 characters'
+        return errors
 
 class Network(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = BlogManager()
     
     def __repr__(self) -> str:
         return f'id: {self.id} - Nombre: {self.name}'
@@ -17,6 +30,7 @@ class Show(models.Model):
     description = TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    objects = BlogManager()
 
     def __repr__(self) -> str:
         return f'id: {self.id} - Título: {self.title} - Release Date: {self.release_date}'
--- tv_app/views.py
+++ tv_app/views.py
@@ -1,33 +1,46 @@
 from django.shortcuts import render, HttpResponse, redirect
+from django.contrib import messages
 from .models import Show, Network
 
 #shows = Show.objects.all()
 #show = Show.objects.get(id=show_id)
 
+# INDEX
 def index(request):
     return redirect('/shows')
 
+# SHOWS
 def shows(request):
     context = {
         'shows' : Show.objects.all()
     }
     return render(request, 'shows.html', context)
 
+# NEW
 def new(request):
     if request.method == 'GET':
         return render(request, 'new.html')
     
     if request.method == 'POST':
         print(request.POST)
-        Show.objects.create(title=request.POST['title'], release_date=request.POST['release_date'], network=request.POST['network'], description=request.POST['description'])
-        return redirect("/")
-    
+        errors = Show.objects.basic_validator(request.POST)
+        
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f"/shows/new")
+        else:
+            Show.objects.create(title=request.POST['title'], release_date=request.POST['release_date'], network=request.POST['network'], description=request.POST['description'])
+            return redirect("/")
+
+# DETAILS
 def details(request, show_id):
     context = {
         'show' : Show.objects.get(id=show_id)
     }
     return render(request, 'details.html', context)
 
+# EDIT
 def edit(request, show_id):
     context = {
         'show' : Show.objects.get(id=show_id)
@@ -37,16 +50,27 @@ def edit(request, show_id):
     
     if request.method == 'POST':
         print(request.POST)
-        show = Show.objects.get(id=show_id)
-        show.title=request.POST['title']
-        show.network=request.POST['network']
-        show.release_date=request.POST['release_date']
-        show.save()
-        return redirect(f"/shows/{show_id}")
+        errors = Show.objects.basic_validator(request.POST)
+        
+        if len(errors) > 0:
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect(f"/shows/{show_id}/edit")
+        else:
+            show = Show.objects.get(id=show_id)
+            show.title=request.POST['title']
+            show.network=request.POST['network']
+            show.description=request.POST['description']
+            show.release_date=request.POST['release_date']
+            show.save()
+            return redirect(f"/shows/{show_id}")
 
+# DELETE
 def delete(request, show_id):
     context = {
         'show' : Show.objects.get(id=show_id)
     }
     Show.objects.get(id=show_id).delete()
     return redirect("/")
+
+#import pdb; pdb.set_trace()
